52bc274892fabf1ea0ef4af51665671404b82ba2
Robin Shen <robin@onedev.io>
2021-11-04 11:31:47 +0800

    Exclude system dependency

--- src/main/java/io/onedev/plugin/maven/PluginUtils.java
+++ src/main/java/io/onedev/plugin/maven/PluginUtils.java
@@ -122,8 +122,7 @@ public class PluginUtils {
 	}
 
 	public static boolean isRuntimeArtifact(Artifact artifact) {
-		return !artifact.hasClassifier() && 
-				(artifact.getScope().equals(Artifact.SCOPE_COMPILE) || artifact.getScope().equals(Artifact.SCOPE_RUNTIME) || artifact.getScope().equals(Artifact.SCOPE_SYSTEM));
+		return !artifact.hasClassifier() && (artifact.getScope().equals(Artifact.SCOPE_COMPILE) || artifact.getScope().equals(Artifact.SCOPE_RUNTIME));
 	}
 	
 	public static void writeClasspath(File file, MavenProject project, RepositorySystem repoSystem, 
